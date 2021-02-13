f4e12032ba09afe7cad2de62792546b6c8ea73d4
Juanjo Valiño <juanjovn@gmail.com>
2021-02-13 20:14:15 +0100

    Change animation durations

--- src/js/sr.js
+++ src/js/sr.js
@@ -3,7 +3,7 @@ export default function() {
   const defaultProps = {
     easing: 'cubic-bezier(0.5, 0, 0, 1)',
     distance: '30px',
-    duration: 1000,
+    duration: 500,
     desktop: true,
     mobile: true
   };
@@ -12,7 +12,7 @@ export default function() {
   ScrollReveal().reveal('.section-title', 
     assignProps(
       {
-        delay:300,distance:'0px', 
+        delay:0,distance:'0px', 
         origin:'bottom'
       }, defaultProps)
   );
@@ -21,7 +21,7 @@ export default function() {
   ScrollReveal().reveal('.hero-title', 
     assignProps(
       { 
-        delay: 500, 
+        delay: 100, 
         origin: window.innerWidth > 768 ? 'left' : 'bottom'
       }, defaultProps)
   );
@@ -29,7 +29,7 @@ export default function() {
   ScrollReveal().reveal('.hero-cta', 
     assignProps(
       {
-        delay: 1000, 
+        delay: 300, 
         origin: window.innerWidth > 768 ? 'left' : 'bottom'
       }, defaultProps)
   );
@@ -38,7 +38,7 @@ export default function() {
   ScrollReveal().reveal('.about-wrapper__image', 
     assignProps(
       {
-        delay: 600,
+        delay: 100,
         origin: 'bottom'
       }, defaultProps)
   );
@@ -46,7 +46,7 @@ export default function() {
   ScrollReveal().reveal('.about-wrapper__info', 
     assignProps(
       {
-        delay: 1000,
+        delay: 300,
         origin: window.innerWidth > 768 ? 'left' : 'bottom'
       }, defaultProps)
   );
@@ -55,7 +55,7 @@ export default function() {
   ScrollReveal().reveal('.project-wrapper__text', 
     assignProps(
       {
-        delay: 500,
+        delay: 100,
         origin: window.innerWidth > 768 ? 'left' : 'bottom'
       }, defaultProps)
   );
@@ -63,7 +63,7 @@ export default function() {
   ScrollReveal().reveal('.project-wrapper__image', 
     assignProps(
       {
-        delay: 1000,
+        delay: 300,
         origin: window.innerWidth > 768 ? 'right' : 'bottom'
       }, defaultProps)
   );
@@ -72,7 +72,7 @@ export default function() {
   ScrollReveal().reveal('.contact-wrapper', 
     assignProps(
       {
-        delay: 800,
+        delay: 200,
         origin: 'bottom'
       }, defaultProps)
   );
